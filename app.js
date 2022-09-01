e99d1e0ee88f382e28057bb7f30f30842c55fa23
coding1984 <105312035+coding1984@users.noreply.github.com>
2022-09-01 23:27:48 +0530

    foodTemplate

---
+++ app.js
@@ -0,0 +1,152 @@
+let slierFlexWidth = document.querySelector('.slierFlex');
+let sliderThree = document.querySelector('.sliderTwo');
+
+let idSliderOne = document.getElementById('sliderOne')
+let idSliderTwo = document.getElementById('sliderTwo')
+let idSliderThree = document.getElementById('sliderThree')
+
+
+let slider = document.querySelectorAll('.slider')
+
+
+// slider.forEach((slides)=>{
+//     slides.addEventListener('click', ()=>{
+//         console.log(slides)
+//         if(slides.className == 'slider sliderOne' ){
+// //            slides.classList.add('firstImage')
+// //            idSliderTwo.style.transform = "translate(350px,0px)"; 
+//             idSliderThree.style.cssText = `
+//             transform: scale(0.5) translate(-1400px,0px);
+            
+
+//             `
+//         }
+//         if(slides.className == 'slider sliderThree' ){
+//             slides.classList.add('thirdImage')
+//         }
+//         if(slides.className == 'slider sliderTwo'){
+//             slides.classList.add('secondImage');
+//         }
+//        slides.style.transform = "translate(350px,0px)";
+//        slides.style.transform = 'scale(1.5)';
+//      
+          
+//        let srcValue = slides.getAttribute('src'); 
+//        sliderThree.setAttribute('src', srcValue)
+//        sliderThree.style.transition = "all 1s"
+//        sliderThree.setAttribute('class', 'slideranimate1')
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+
+})
+    
+})
